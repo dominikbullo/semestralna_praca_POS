ed082120b45cfe8ba3319ff5f179d69c0a74e5c6
Dominik <dominobullo@gmail.com>
2019-12-28 19:13:50 +0100

    Server basic setup

---
+++ semestralna_praca_POS_server/ServerConnection.cpp
@@ -0,0 +1,49 @@
+#include "ServerConnection.h"
+#define REG 0
+#define LOG 1
+#define SND 2
+string username;
+string password;
+
+ServerConnection::ServerConnection() {
+    int sockfd, newsockfd, newsockf2;
+
+    socklen_t cli_len;
+    struct sockaddr_in serv_addr, cli_addr;
+
+    int i = 0;
+    char buffer[256];
+    int n;
+
+    //vyčistenie (vynulovanie) a inicializácia sieťovej adresy
+
+    bzero((char*) &serv_addr, sizeof (serv_addr));
+
+    //nastavenie pre adresovanie v doméne INET
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(20055);
+
+    //tvorba samotného socketu "sockfd"
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    //priradenie adresy socketu (aj s overením)
+
+    if (bind(sockfd, (struct sockaddr*) &serv_addr, sizeof (serv_addr)) < 0) {
+        perror("CHYBA! - nepodarilo sa priradiť adresu");
+    }
+
+    //umožníme pripojenie klienta na socket "sockfd",
+    //druhý parameter je dĺžka fronty na pripojenie k "sockfd"
+
+    listen(sockfd, 10);
+    cli_len = sizeof (cli_addr);
+
+    vector<thread> *threads = new vector<thread>();
+}
+
+ServerConnection::~ServerConnection() {
+}
+
+
+
+
---
+++ semestralna_praca_POS_server/ServerConnection.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <vector>
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+
+#ifndef SERVERCONNECTION_H
+#define SERVERCONNECTION_H
+
+using namespace std;
+
+class ServerConnection {
+public:
+    ServerConnection();
+    virtual ~ServerConnection();
+};
+
+#endif /* SERVERCONNECTION_H */
+
