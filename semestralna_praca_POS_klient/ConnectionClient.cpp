ef0fb4ba1359e40ec3bc74c771d549c0fe2d51f7
janak <janak@isw.sk>
2019-12-28 20:13:23 +0100

    client configuration

---
+++ semestralna_praca_POS_klient/ConnectionClient.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   ConnectionClient.cpp
+ * Author: Lukáš Jaňák
+ * 
+ * Created on December 28, 2019, 8:12 PM
+ */
+
+#include "ConnectionClient.h"
+
+ConnectionClient::ConnectionClient() {
+}
+
+ConnectionClient::ConnectionClient(const ConnectionClient& orig) {
+}
+
+ConnectionClient::~ConnectionClient() {
+}
+
---
+++ semestralna_praca_POS_klient/ConnectionClient.h
@@ -0,0 +1,14 @@
+#ifndef CONNECTIONCLIENT_H
+#define CONNECTIONCLIENT_H
+
+class ConnectionClient {
+public:
+    ConnectionClient();
+    ConnectionClient(const ConnectionClient& orig);
+    virtual ~ConnectionClient();
+private:
+
+};
+
+#endif /* CONNECTIONCLIENT_H */
+
