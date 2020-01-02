86cd4e2531a7b3594f4750241d43b9e04cbeb9c9
Dominik <dominobullo@gmail.com>
2020-01-02 17:30:24 +0100

    message type as global string variable

--- semestralna_praca_POS_server/ConnectionServer.cpp
+++ semestralna_praca_POS_server/ConnectionServer.cpp
@@ -6,6 +6,9 @@
 
 using namespace std;
 
+const char *MSSG_TRUE = "0;T";
+const char *MSSG_FALSE = "0;F";
+
 ConnectionServer::ConnectionServer() {
     // vytvorím si štruktúry
     onlineUsers = new vector<ConnectedUser*>();
@@ -66,7 +69,7 @@ ConnectionServer::ConnectionServer() {
 
 void ConnectionServer::controlUser(int socket) {
     cout << "Control user on socket " << socket << endl;
-    
+
     ConnectedUser* user;
 
     char buffer[256];
@@ -84,80 +87,87 @@ void ConnectionServer::controlUser(int socket) {
         //načítava do buffera z newsockfd
         response = read(socket, buffer, 255);
 
-        cout << endl << response << endl;
-        cout << endl << string(buffer) << endl;
+        //        cout << endl << response << endl;
+        //        cout << endl << string(buffer) << endl;
 
         if (response == 0 || response == -1) {
             break;
         }
 
-        cout << endl << response << endl;
-        cout << endl << string(buffer) << endl;
+        //        cout << endl << response << endl;
+        //        cout << endl << string(buffer) << endl;
 
         // parser spravy 
         messageReader->readMsg(parsedMsg, string(buffer)); //zavoal parsovac spravy
         
+        // TODO try catch
+        int mssgType = stoi(parsedMsg->at(0));
+        auto mssgSender = stoi(parsedMsg->at(1));
+        auto mssgBody = parsedMsg->at(2);
+        
         // vypis spravu
-        cout << endl << parsedMsg->at(1) << " " << parsedMsg->at(2) << endl;
+        cout << endl << mssgSender << " " << mssgBody << endl;
 
-        switch (stoi(parsedMsg->at(0))) {
+
+        switch (mssgType) {
+                cout << "Received message type " << mssgType << endl;
             case 1:
                 // register user
-                cout << "Received message type 1" << endl;
                 cout << "Need to register user" << endl;
                 if (registerUser(*parsedMsg)) {
-                    msg = "0;T";
+                    cout << "Sending " << MSSG_TRUE << endl;
+                    msg = MSSG_TRUE;
 
                 } else {
-                    msg = "0;F";
+                    msg = MSSG_FALSE;
                 }
-
-                cout << "sending response " << msg << endl;
-                n = write(socket, msg, strlen(msg) + 1); //zasiela správu "msg" klientovi                
                 break;
             case 2:
                 // login user
-                cout << "Received message type 2" << endl;
                 cout << "Need to login user" << endl;
                 int i;
                 cout << "logging user on socket " << socket << endl;
                 if ((i = loginUser(*parsedMsg, socket)) != -1) {
                     pozicia = i;
                     user = allUsers->at(i);
-                    msg = "0;T";
+                    msg = MSSG_TRUE;
                 } else {
-                    msg = "0;F";
+                    msg = MSSG_FALSE;
                 }
-
-                cout << "sending response " << msg << endl;
-                n = write(socket, msg, strlen(msg) + 1); //zasiela správu "msg" klientovi
                 break;
             case 3:
                 // received message
-                cout << "Received message type 3" << endl;
                 cout << "Received messsage to user" << endl;
+                cout << "Not implemented yet!" << endl;
                 break;
             case 4:
                 // show contact
-                cout << "Received message type 4" << endl;
+                cout << "Show conctacts" << endl;
+                cout << "Not implemented yet!" << endl;
                 break;
             case 5:
                 // add contact
-                cout << "Received message type 5" << endl;
+                cout << "Add concact" << endl;
+                cout << "Not implemented yet!" << endl;
                 break;
             case 6:
                 // delete contact
-                cout << "Received message type 6" << endl;
+                cout << "Delete contact" << endl;
+                cout << "Not implemented yet!" << endl;
                 break;
             case 10:
                 // make contact offline
-                cout << "Received message type 10" << endl;
+                cout << "Make contact offline" << endl;
+                cout << "Not implemented yet!" << endl;
                 break;
             default:
                 cout << "Not implemented yet!" << endl;
                 break;
         }
     }
+
+    cout << "Sending response " << msg << endl;
+    n = write(socket, msg, strlen(msg) + 1); //zasiela správu "msg" klientovi     
 
     cout << "Closing socket" << endl;
     close(socket);
